68d10b09726f2011bad8c9cbccc4babb66e8335e
Vira <vira.romanko99@gmail.com>
2019-04-16 23:44:06 -0400

    gallery home page

--- js/main.js
+++ js/main.js
@@ -18,6 +18,64 @@
   	searchButton.classList.toggle("expanded");
   }
 
+
+
+  //image gallery
+
+  let slider = document.querySelectorAll(".slide"),
+  leftArrow = document.querySelector("#arrow-left"),
+  rightArrow = document.querySelector("#arrow-right"),
+  currentSlide = 0; 
+
+//reset all the images
+function reset() {
+  for (let i = 0; i < slider.length; i++) {
+    slider[i].style.display = "none";
+  }
+}
+
+// first slider to show
+function startSlide() {
+  reset();
+
+  slider[0].style.display = "block";
+}
+
+// before slider
+function slideLeft() {
+  reset();
+  console.log("previous slide");
+  slider[currentSlide - 1].style.display = "block";
+  currentSlide--;
+}
+
+// After slider snow next one
+function slideRight() {
+  reset();
+  console.log("next slide");
+  slider[currentSlide + 1].style.display = "block";
+  currentSlide++;
+}
+//click on left arrow
+leftArrow.addEventListener("click", function() {
+  if (currentSlide === 0) {
+    currentSlide = slider.length;
+  }
+  slideLeft();
+  console.log("you clicked left arrow");
+});
+
+// click on right arrow
+rightArrow.addEventListener("click", function() {
+  if (currentSlide === slider.length - 1) {
+    currentSlide = -1;
+  }
+  slideRight();
+  console.log("you clicked right arrow");
+});
+
+startSlide();
+
   button.addEventListener("click", hamburgerMenu, false);
   searchButton.addEventListener("click", SearchBox, false);
 
